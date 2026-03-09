8a3a93a543d9adb3e0733737dd7f3a9869292465
Ronny Brenes <rjba08@gmail.com>
2026-03-09 15:04:22 -0600

    chore: remove unnecessary files (test.sql, make_eb_zip.py, unused .http files)

--- test.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Incidents (Id, Title, Description, CreatedAt)
-VALUES 
-(NEWID(), 'Server down', 'The main server is not responding', GETDATE()),
-(NEWID(), 'Database error', 'Unable to connect to DB', GETDATE());
